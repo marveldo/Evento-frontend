7f1c4b009a9569c2db16bcc116c2996d17205ef8
utibe solomon <utibesolomon6@gmail.com>
2024-11-22 10:40:00 +0100

    Updated Metadata api

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,32 @@ import ProgressProvider from "./componenets/progress/nextprogressbar";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 import { Websockethook } from "@/SessionWrapper/WebsocketHook";
-import Head from "next/head";
 
-// export const metadata: Metadata = {
-//   title: "Event application",
-//   description: "An event app where events are scheduled and users can register for events",
-// };
+export const metadata: Metadata = {
+  title: "Event application",
+  description: "An event app where events are scheduled and users can register for events",
+  keywords : ["events", 'evento' , 'evento-frontend', 'evented', 'Event'],
+  openGraph : {
+    title : 'Evento',
+    description : 'An Event App for planning Events and registering for Events',
+    url : 'https://evento-frontend.vercel.app/',
+    images : [
+      {
+        url: "/favicon.ico",
+        width: 32,
+        height: 32,
+        alt: "Event Platform",
+      },
+    ]
+  },
+  twitter : {
+    card: "summary_large_image",
+    title: "Evento",
+    description: "An Event App for planning Events and registering for Events",
+    images: ["/favicon.ico"],
+  },
+  
+};
 
 
 export default async function RootLayout({children}:Readonly<{children: React.ReactNode;}>) {
@@ -23,16 +43,6 @@ export default async function RootLayout({children}:Readonly<{children: React.Re
     <SessionWrapper session={session} >
       <Websockethook>
       <html lang="en">
-      <Head>
-        <title>Home - Evento</title>
-        <meta name="description" content="An event app for registering for events and scheduling events" />
-        <meta name="keywords" content="events, evento, evented evento-frontend" />
-        <meta property="og:title" content="Home - Evento" />
-        <meta property="og:description" content="An Event application." />
-        <meta property="og:image" content="/favicon.ico" />
-        <meta property="og:url" content="https://evento-frontend.vercel.app/" />
-        <link rel="canonical" href="https://evento-frontend.vercel.app/" />
-      </Head>
       <body className="font-work flex flex-col min-h-screen bg-white">
       <Navbar/>
       <ProgressProvider>{children}</ProgressProvider>
